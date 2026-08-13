3a672086731208d1723baffda523731b5a8feef6
68160224 <68160224@go.buu.ac.th>
2026-08-13 08:51:04 +0700

    update main

--- src/main/java/loop/oxgame03/OXGame03.java
+++ src/main/java/loop/oxgame03/OXGame03.java
@@ -11,6 +11,7 @@ package loop.oxgame03;
 public class OXGame03 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Game game = new Game();
+        game.play();
     }
 }
