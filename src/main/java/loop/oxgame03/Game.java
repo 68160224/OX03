d9fefe0303d078327de3863dc23d30f9cc28ae5e
68160224 <68160224@go.buu.ac.th>
2026-08-13 08:46:38 +0700

    variable Game

---
+++ src/main/java/loop/oxgame03/Game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loop.oxgame03;
+
+/**
+ *
+ * @author Windows
+ */
+public class Game {
+    private Board board;
+    private Player x;
+    private Player o;
+
+}
