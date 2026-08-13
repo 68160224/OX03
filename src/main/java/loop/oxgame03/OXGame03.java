173a43de166f122b94dcf9d77c29ef2fe1a84383
68160224 <68160224@go.buu.ac.th>
2026-08-13 08:31:21 +0700

    Initial commit

---
+++ src/main/java/loop/oxgame03/OXGame03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package loop.oxgame03;
+
+/**
+ *
+ * @author Windows
+ */
+public class OXGame03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
